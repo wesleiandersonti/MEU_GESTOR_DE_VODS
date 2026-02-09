0561fc82e18fab1518f4e3ecd4c0c1ac9938dc6f
Weslei <wesleiandersonti@gmail.com>
2026-02-09 15:33:52 -0300

    feat: LisoFlix Pro com config localStorage, HTTPS, lazy loading e cast para rede local

---
+++ playlistfinder_1_22/2. Whois.bat
@@ -0,0 +1,11 @@
+@rem %1 original Playlist
+@rem %2 masked Playlist
+@rem %3 new Playlist
+@rem %4 masked Address
+@rem %5 new IP-Address
+@rem %6 new Port
+@rem %7 new Address
+@rem %8 Country
+@rem %9 Network-Name
+
+start http://whoiz.herokuapp.com/lookup?url=%5
